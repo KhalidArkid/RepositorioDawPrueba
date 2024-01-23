e83b3108e8193ef345174e8ffb7094ef22915b3f
KhalidArkid <khalid.arkid@gmail.com>
2024-01-23 21:38:14 +0100

    mucho texto

--- python-UF2/llibreria.py
+++ python-UF2/llibreria.py
@@ -1,13 +1,13 @@
 AFEGIR = 1
 MOSTRAR = 2
 ELIMINAR = 3
-PRESTAMO = 4
+PRESTAR = 4
 MOSTRAR_PRESTAMOS = 5
-DEVOLVER_PRESTAMO = 6
+DEVOLVER = 6
 SORTIR = 7
 
-llibreria = []
-prestamos = []
+llibreria = {}
+prestamos = {} 
 fin_programa = False
 
 while not fin_programa:
@@ -15,58 +15,58 @@ while not fin_programa:
     print(str(AFEGIR) + ". Afegir un llibre")
     print(str(MOSTRAR) + ". Mostrar llista dels llibres")
     print(str(ELIMINAR) + ". Eliminar un llibre")
-    print(str(PRESTAMO) + ". Prestar un llibre") 
+    print(str(PRESTAR) + ". Prestar un llibre")
     print(str(MOSTRAR_PRESTAMOS) + ". Mostrar llibres prestats")
-    print(str(DEVOLVER_PRESTAMO) + ". Retornar un llibre"
+    print(str(DEVOLVER) + ". Retornar un llibre")
     print(str(SORTIR) + ". Sortir")
-    
+
     opcion = int(input("Escull una opció (1-7): "))
 
     if opcion == AFEGIR:
         nom = input("Introdueix el nom del llibre: ")
-        llibreria.append(nom)
-        print("Llibre " + nom + " afegit.")
+        llibreria[nom] = "llibre interesant"
+        print("Nom: " + str(nom) + " registrat")
 
     if opcion == MOSTRAR:
         if llibreria:
-            for nom in llibreria.keys():
-                print("Nom: " + nom)
+            for nom, detall in llibreria.items():
+                print("Nom: {}, Detall: {}".format(nom, detall))
         else:
             print("No hi ha llibres registrats.")
 
     if opcion == ELIMINAR:
         nom = input("Introdueix el nom del llibre que vols eliminar: ")
-        if llibreria.count(nom) > 0:
-            llibreria.remove(nom)
-            print("Llibre " + nom + " eliminat.")
+        if nom in llibreria:
+            del llibreria[nom]
+            print("Nom: " + str(nom) + " eliminat")
         else:
-            print("El llibre " + nom + " no existeix a la llista.")
+            print("El llibre " + str(nom) + " no existeix a la llista.")
 
-    if opcion == PRESTAMO:
+    if opcion == PRESTAR:
         nom = input("Introdueix el nom del llibre que vols prestar: ")
         if nom in llibreria and nom not in prestamos:
-            prestamos[nom] = "Persona que presta" 
-            print("Llibre " + nom + " prestat.")
-        if nom in prestamos:
-            print("El llibre " + nom + " ja està prestat.")
+            prestamos[nom] = "Prestat"
+            print("Llibre " + str(nom) + " prestat.")
+        elif nom in prestamos:
+            print("El llibre " + str(nom) + " ja està prestat.")
         else:
-            print("No es pot prestar el llibre " + nom + " ja que no existeix a la llista.")
+            print("No es pot prestar el llibre " + str(nom) + " ja que no existeix a la llista.")
 
-    lif opcion == MOSTRAR_PRESTAMOS:
+    if opcion == MOSTRAR_PRESTAMOS:
         if prestamos:
             for nom, persona in prestamos.items():
                 print("Nom: {}, Prestat a: {}".format(nom, persona))
         else:
-            print("No hi ha llibres prestats.")
+            print("No hi ha llibres actualment prestats.")
 
-    elif opcion == DEVOLVER_PRESTAMO:
+    if opcion == DEVOLVER:
         nom = input("Introdueix el nom del llibre que vols retornar: ")
         if nom in prestamos:
-            del prestamos[nom]
-            print("Llibre " + nom + " retornat.")
+            prestamos[nom] = None 
+            print("Llibre " + str(nom) + " retornat.")
         else:
-            print("El llibre " + nom + " no està actualment prestat.")
-    
+            print("El llibre " + str(nom) + " no està actualment prestat o ja ha estat retornat.")
+
     if opcion == SORTIR:
         print("Programa tancat.")
         fin_programa = True
