2b9df9d0ed5f14093cc7309ab036fd29f0d6741a
Khalid <khalid.arkid@gmail.com>
2024-02-01 12:29:25 +0100

    me rallao, no se me dan bien los def, tendré que practicarlo más

--- python-UF2/pizza.py
+++ python-UF2/pizza.py
@@ -1,24 +1,31 @@
 '''
 Imagineu-vos que heu estat contractats per dissenyar el sistema de gestió d'una pizzeria. 
-L'objectiu és desenvolupar un conjunt de funcions que permetin gestionar diferents tasques relacionades amb el funcionament de la pizzeria. Això inclou la presa de comandes, la preparació de pizzes, la gestió de l'estoc d'ingredients i el processament dels pagaments.
+l'objectiu és desenvolupar un conjunt de funcions que permetin gestionar diferents tasques relacionades amb el funcionament de la pizzeria. Això inclou la presa de comandes, la preparació de pizzes, la gestió de l'estoc d'ingredients i el processament dels pagaments.
 Recordeu fer servir disseny top down, i mantenir un baix acoblament i una alta cohesió.
 '''
 
 COMANDA = 1
-ELIMINAR = 2
-LLISTAR = 3
-SALIR = 4
+PREPARACIO = 2
+ESTOC = 3
+PAGAMENT = 4
+SALIR = 5
 
-Pizza = []
-nombre = ""
-fin_programa = False
+pizza = []
+fin_programa= False
 
-while fin_programa == False:
+while not fin_programa:
 
-    def Comanda():
-        print("\nAccions disponibles:")
-        print(COMANDA + ". Ingredients")
-        print(ELIMINAR + ". Eliminar un ingrediente")
-        print(LLISTAR + ". Llista de tots els ingredients")
-        print(SALIR + ". Sortir")
-adas
+    print("\nAccions disponibles:")
+    print(str(COMANDA) + ". Ingredients")
+    print(str(PREPARACIO) + ". Eliminar un ingrediente")
+    print(str(ESTOC) + ". Llista de tots els ingredients en estoc")
+    print(str(PAGAMENT) + ". efectiu o tarjeta")
+    print(str(SALIR) + ". Sortir")
+    Opcion = int(input("Escull una opció (1-5): "))
+    
+    def LACOMANDA():
+    pizza = int(input(" Que pizza vols?: 1. Pizza 4 quesos, 2. pizza carbonara, 3. pizza vegetal"))     
+    if pizza == 1:
+        print("Els ingredients de " + pizza + " son:")
+
+    print(LACOMANDA)
