1b6a10aa65a2298f8cf121c3906e36661f9248a9
Khalid <khalid.arkid@gmail.com>
2024-02-02 10:32:49 +0100

    q tock

--- python-UF2/pizza.py
+++ python-UF2/pizza.py
@@ -1,31 +1,24 @@
 '''
 Imagineu-vos que heu estat contractats per dissenyar el sistema de gestió d'una pizzeria. 
-l'objectiu és desenvolupar un conjunt de funcions que permetin gestionar diferents tasques relacionades amb el funcionament de la pizzeria. Això inclou la presa de comandes, la preparació de pizzes, la gestió de l'estoc d'ingredients i el processament dels pagaments.
+L'objectiu és desenvolupar un conjunt de funcions que permetin gestionar diferents tasques relacionades amb el funcionament de la pizzeria. Això inclou la presa de comandes, la preparació de pizzes, la gestió de l'estoc d'ingredients i el processament dels pagaments.
 Recordeu fer servir disseny top down, i mantenir un baix acoblament i una alta cohesió.
 '''
 
-COMANDA = 1
-PREPARACIO = 2
-ESTOC = 3
-PAGAMENT = 4
-SALIR = 5
+'''
+1.1 Gestión de Pizzeria
+1.2 Recibir pedido
+1.3 Preparación Pizza 
+1.4 Gestión de stock
+1.5 Pagos 
+'''
+'''
+1.1.1 (Recibir que quiere el cliente)
+1.1.2 Verificación de stock
+1.2.1 Cocina
+1.2.2 Verificar calidad
+1.3.1 Actualizar stock
+1.3.2 Abastecer
+1.4.1 Costes
+1.4.2 Imprimir factura
 
-pizza = []
-fin_programa= False
-
-while not fin_programa:
-
-    print("\nAccions disponibles:")
-    print(str(COMANDA) + ". Ingredients")
-    print(str(PREPARACIO) + ". Eliminar un ingrediente")
-    print(str(ESTOC) + ". Llista de tots els ingredients en estoc")
-    print(str(PAGAMENT) + ". efectiu o tarjeta")
-    print(str(SALIR) + ". Sortir")
-    Opcion = int(input("Escull una opció (1-5): "))
-    
-    def LACOMANDA():
-    pizza = int(input(" Que pizza vols?: 1. Pizza 4 quesos, 2. pizza carbonara, 3. pizza vegetal"))     
-    if pizza == 1:
-        print("Els ingredients de " + pizza + " son:")
-
-    print(LACOMANDA)
+'''
