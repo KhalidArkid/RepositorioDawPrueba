29aa59021da37a86ee9dbef904f5e01b76c4350d
Khalid <khalid.arkid@gmail.com>
2024-02-13 13:31:39 +0100

    _------------------------------------------------------CORRECCION HECHA -------------------------------------------------------------------------------------

--- python-UF2/observatori.py
+++ python-UF2/observatori.py
@@ -5,34 +5,67 @@ CORRECCIÓN
 temperaturas = []
 mes = 1
 dia = 1
+fi = False
 
 # Problema general
 def inici():
-    ...
+    while not fi:
+        mostrar_menu()
+        tractar_opcion()
 
 # Primer nivel de descomposicion
 def mostrar_menu():
-    ...
+    print("Benvingut al registre de temperatures")
+    print("-------------------------------------")
+    print("[RT] Registrar temperatures setmanas.")
+    print("[MJ] Consultar temperatura mitjana.")
+    print("[DF] Consultar diferencia maxima.")
+    print("[FI] Sortir")
 
 def tractar_opcion():
-    ...
+    opcio = input("Opcio: ")
+    if opcio.casefold() == "RT".casefold():
+        registro_temperaturas_semanales()
+    elif opcio.casefold() == "MJ".casefold():
+        mostrar_media()
+    elif opcio.casefold() == "DF".casefold():
+        mostrar_diferencia()
+    elif opcio.casefold() == "FI".casefold():
+        finalizar_ejecucion()
+    else:
+        print("Opcio incorrecta.")
 
 # Segundo nivel de descomposicion
 def registro_temperaturas_semanales():
-    ...
+    if (len(temperaturas) + 7) > (52*7):
+        print("Ja tenim totes les setmanes introduides no podem introduir mes.")
+    else:
+        leer_temperaturas_teclado()
+        incrementar_fecha()
 
 def mostrar_media():
-    ...
+    if len(temperaturas) > 0:
+        media = calcular_media()
+        print("Fins avui", end = " ")
+        mostrar_fecha()
+        print("la mitjana a estat de", media, "graus.")
+    else:
+        print("No hi ha temperatures registrades")
 
 def mostrar_diferencia():
-    ...
+    if len(temperaturas) > 0:
+        diferencia = calcular_diferencia()
+        print("Fins avui", end = " ")
+        mostrar_fecha()
+        print("la diferencia maxima ha estat de", diferencia, "graus.")
 
 def finalizar_ejecucion():
-    ...
+    global fi
+    fi = True
 
 # Tercer nivel de descomposicion
 def leer_temperaturas_teclado():
-    lector = input("Escribe las temperaturas de esta semana: ")
+    lector = input("Escriu les temperatures d'aquesta setmana:\n ")
     for temperatura in lector.split():
         temperaturas.append(float(temperatura.replace(',','.')))
 
@@ -40,15 +73,22 @@ def calcular_media():
     suma = 0
     for temperatura in temperaturas:
         suma += temperatura
+
     return suma/len(temperaturas)
 
 def calcular_diferencia():
-    #temperatura minima
-    #temperatura maxima
-    #recorrer todas las temperaturas e ir modificando
+    minima = temperaturas[0]
+    maxima = temperaturas[0]
+    for temperatura in temperaturas:
+        if minima > temperatura:
+            minima = temperatura
+        if  maxima < temperatura:
+            maxima = temperatura
+    diferancia = maxima - minima
+    return diferancia
 
 def mostrar_fecha():
-    print(dia, "de", end=' ')
+    print(dia, "de", end= ' ')
     if mes == 1:
         print("Enero")
     elif mes == 2:
@@ -75,6 +115,7 @@ def mostrar_fecha():
         print("Diciembre")
 
 def incrementar_fecha():
+    global mes, dia
     dias_mes_actual = 0
     if mes == 2:
         dias_mes_actual = 28
@@ -89,4 +130,4 @@ def incrementar_fecha():
         if mes > 12:
             mes = 1
 
-mostrar_fecha()
+inici()
