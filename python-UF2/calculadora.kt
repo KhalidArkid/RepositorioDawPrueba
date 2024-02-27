6210988a239111a513d31830fb747d6e398243cb
Khalid <khalid.arkid@gmail.com>
2024-02-27 12:31:57 +0100

    printeo y main

--- python-UF2/calculadora.kt
+++ python-UF2/calculadora.kt
@@ -20,3 +20,15 @@ fun multiplicar(a: Int, b: Int): Int {
 fun dividir(a: Int, b: Int): Int {
     return a / b
 }
+
+main(){
+    suma = suma(a, b)
+    restar = restar(a, b)
+    multiplicar = multiplicar(a, b)
+    dividir = dividir(a, b)
+}
+
+println (suma)
+println (restar)
+println (multiplicar)
+println (dividir)
