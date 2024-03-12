221e0a0d89eaf080a346fddc7bcdb797690a761c
Khalid <khalid.arkid@gmail.com>
2024-03-12 14:17:36 +0100

    HECHO (kotlin) (clases)

---
+++ python-UF2/classes.kt
@@ -0,0 +1,32 @@
+// Coordenada inicial
+class Coordenada {
+    var x: Int = 0
+    var y: Int = 0
+}
+
+// Programa principal
+fun main() {
+    val coordenada = Coordenada()
+    coordenada.moure_dreta()
+    println("Nova coordenada després de moure a la dreta: (${coordenada.x}, ${coordenada.y})")
+
+    coordenada.moure_amunt()
+    println("Nova coordenada després de moure amunt: (${coordenada.x}, ${coordenada.y})")
+}
+
+// Funciones de movimiento
+fun Coordenada.moure_dreta() {
+    x += 1
+}
+
+fun Coordenada.moure_esquerra() {
+    x -= 1
+}
+
+fun Coordenada.moure_amunt() {
+    y += 1
+}
+
+fun Coordenada.moure_avall() {
+    y -= 1
+}
