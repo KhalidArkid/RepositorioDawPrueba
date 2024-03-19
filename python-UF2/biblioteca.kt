aa969f68be1b075f9f69a45eda041d3054d1a9c1
Khalid <khalid.arkid@gmail.com>
2024-03-19 12:25:06 +0100

    correcciones

--- python-UF2/biblioteca.kt
+++ python-UF2/biblioteca.kt
@@ -1,21 +1,84 @@
-class Llibre (var nom: String, var autor: String, var quantitat: Int){
-    fun informacio() {}
+class Libro(val titulo: String, val autor: String, var ejemplaresDisponibles: Int) {
+
+    fun prestar() {
+        if (ejemplaresDisponibles > 0) {
+            ejemplaresDisponibles--
+            println("Libro prestado: $titulo, Autor: $autor")
+        } else {
+            println("No hay ejemplares disponibles de $titulo")
+        }
+    }
+
+    fun devolver() {
+        ejemplaresDisponibles++
+        println("Libro devuelto: $titulo, Autor: $autor")
+    }
+
+    fun informacion() {
+        println("Título: $titulo, Autor: $autor, Ejemplares Disponibles: $ejemplaresDisponibles")
+    }
 }
 
+class Socio(val nombre: String, val apellido: String, val numeroSocio: Int) {
 
-class main() {
-    val llibre1 = Llibre("Paco", "Zawar", 5)
-    val llibre2 = Llibre("PAMPAM", "Adil", 2)
-    
-    val soci1 = Soci("Pedro", "Panflina", "123")
-    val soci2 = Soci("Marco", "Polo", "456")
-    
-    val prestec1 = Prestec(llibre1, soci1, "2024-03-14")
-    val prestec2 = Prestec(llibre2, soci2, "2023-03-14")
+    fun solicitarPrestamo(libro: Libro, fechaPrestamo: String) {
+        println("Préstamo solicitado por $nombre $apellido, Número de Socio: $numeroSocio")
+        val prestamo = Prestamo(libro, this)
+        prestamo.registrarPrestamo(fechaPrestamo)
+    }
 
-    prestec1.registrarPrestec()
-    llibre1.informacio()
-    soci2.informacio()
-    prestec1.retornarPrestec()
-    llibre1.informacio()
+    fun devolverPrestamo(libro: Libro) {
+        println("Devolución de préstamo por $nombre $apellido, Número de Socio: $numeroSocio")
+        val prestamo = Prestamo(libro, this)
+        prestamo.devolverPrestamo()
+    }
+
+    fun informacion() {
+        println("Nombre: $nombre, Apellido: $apellido, Número de Socio: $numeroSocio")
+    }
+}
+
+class Prestamo(val libro: Libro, val socio: Socio)
+{
+    fun registrarPrestamo(fechaPrestamo: String) {
+        println("Préstamo registrado - Libro: ${libro.titulo}, Socio: ${socio.nombre} ${socio.apellido}, Fecha: $fechaPrestamo")
+        libro.prestar()
+    }
+
+    fun devolverPrestamo() {
+        println("Devolución de préstamo - Libro: ${libro.titulo}, Socio: ${socio.nombre} ${socio.apellido}")
+        libro.devolver()
+    }
+
+    fun informacion() {
+        println("Libro: ${libro.titulo}, Socio: ${socio.nombre} ${socio.apellido}")
+    }
+}
+
+class App {
+	fun exec() {
+		// Crear libros
+		var libro1: Libro = Libro("LOTR", "J.R.R. Tolkien", 5)
+		var libro2: Libro = Libro("1984", "George Orwell", 3)
+
+		// Crear socios
+		val socio1 = Socio("Clark", "Kent", 101)
+		val socio2 = Socio("Bruce", "Wayne", 102)
+
+		// Realizar acciones
+		libro1.informacion()
+		socio1.solicitarPrestamo(libro1, "2024-03-01")
+		libro1.informacion()
+		libro2.informacion()
+		socio2.solicitarPrestamo(libro2, "2024-03-01")
+		libro2.informacion()
+		libro1.informacion()
+		socio1.devolverPrestamo(libro1)
+		libro1.informacion()
+
+	}
+}
+
+fun main() {
+	App().exec()
 }
